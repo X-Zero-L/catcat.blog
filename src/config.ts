cd5db3f2a46aabd903ae6000e86682236e55d839
Yuri-NagaSaki <sayyikue@gmail.com>
2025-08-27 15:12:47 +0800

    添加友链页面及相关配置，更新导航栏以包含友链链接，并在国际化文件中添加友链的翻译。同时，创建友链页面内容，展示优质博客和网站的信息，增强站点的社交互动性。

--- src/config.ts
+++ src/config.ts
@@ -45,6 +45,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
+		LinkPreset.Friends,
 		{
 			name: "GitHub",
 			url: "https://github.com/Yuri-NagaSaki", // Internal links should not include the base path, as it is automatically added
