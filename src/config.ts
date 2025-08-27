9d8f2bd36e2bd599d5e68748a9d5afe680f7dad4
Yuri-NagaSaki <sayyikue@gmail.com>
2025-08-27 17:39:31 +0800

    更新配置文件，调整主题色为黄色，并启用横幅功能，更新横幅图片链接以提升页面视觉效果和用户体验。

--- src/config.ts
+++ src/config.ts
@@ -13,12 +13,12 @@ export const siteConfig: SiteConfig = {
 		"猫猫博客致力于为动画爱好者提供高质量的BDRIP和Blu-ray Remux资源，分享Emby服务器使用指南、PT下载经验、VPS推荐和服务器测评等内容。",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 60, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "https://s3.catcat.blog/background.avif", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -27,7 +27,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	toc: {
-		enable: true, // Display the table of contents on the right side of the post
+		enable: false, // Display the table of contents on the right side of the post
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
